4e718af234d96f8ed99bbeb037426693a8888a36
Shafeef Omar <shafeefomar@Shafeefs-MacBook-Pro.local>
2017-05-23 17:35:39 +0530

    Form validation completed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-var app=angular.module('minmax',[]);
-app.controller('MinMaxCtrl',function($scope){
-	$scope.formModel={};                         //data-binding
+// create angular app
+var validationApp = angular.module('validationApp', []);
 
+// create angular controller
+validationApp.controller('mainController', function($scope) {
 
-});
+// function to submit the form after all validation has occurred			
+	$scope.submitForm = function() {
+
+// check to make sure the form is completely valid
+		if ($scope.userForm.$valid) {
+				alert('Form is successfully submitted');
+			}
+
+		};
+
+	});
